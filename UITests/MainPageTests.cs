96b92c2cf7ab7c9aad8de3f3ca94491d69f80c27
Luce Carter <luce@lucyscarter.co.uk>
2017-12-18 13:11:16 +0000

    @

---
+++ UITests/MainPageTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Menopaws.UITests
+{
+	[TestFixture(Platform.Android)]
+	//[TestFixture(Platform.iOS)]
+	public class MainPageTests
+	{
+		IApp app;
+		Platform platform;
+
+		public MainPageTests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+		    string apkpath = "C:\\Users\\Luce\\Desktop\\Development\\menopaws\\UITests\\co.uk.menopaws.Menopaws.apk";
+		    app = ConfigureApp.Android.Debug().ApkFile(apkpath).StartApp();
+        }
+
+		[Test]
+		public void AddHotFlushButtonExists()
+		{
+			AppResult[] results = app.WaitForElement(c =>  c.Button("ADD HOT FLUSH"));
+			app.Screenshot("Hot Flush Button.");
+
+			Assert.IsTrue(results.Any());
+		}
+	}
+}
